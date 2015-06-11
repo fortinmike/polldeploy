f1a5e6852c32aece4602972d0ef7de9c0d144d21
Michaël Fortin <michael@simbioz.com>
2015-06-11 08:35:46 -0400

    Refined DSL sources a bit (no implementation yet).

--- resources/polldeploy-config.rb
+++ resources/polldeploy-config.rb
@@ -1,6 +1,11 @@
-teamcity = PollDeploy::TeamCitySource.new("http://teamcity.mydomain.com", "username", "password")
+source :teamcity do |s|
+  s.type = TeamCitySource
+  s.endpoint = "http://teamcity.mydomain.com"
+  s.username = "username"
+  s.password = "password"
+end
 
-deploy_from teamcity, { :project => "MyProject", :branch => "master" } do |artifact|
+deploy_from :teamcity, { :project => "MyProject", :branch => "master" } do |source, artifact|
   File.open(File.expand_path("~/deployments.txt", 'a')) do |file|
     file.puts("Deploying artifact #{artifact.path}")
   end
