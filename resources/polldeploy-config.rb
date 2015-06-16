d014fcd4ea9c266c00107939faa236e4d4e41a9d
Michaël Fortin <michael@simbioz.com>
2015-06-15 21:47:35 -0400

    Deploy TeamCity source options are exact build locator keys/values as defined in TeamCity REST API documentation, which means we can filter builds any way we like from the config file.

--- resources/polldeploy-config.rb
+++ resources/polldeploy-config.rb
@@ -4,6 +4,6 @@ source :teamcity, TeamCitySource do |s|
   s.password = "password"
 end
 
-deploy "MyProject", :teamcity, { :project_id => "MyProject", :branch => "master" } do |d|
+deploy "MyProject", :teamcity, { :project => "MyProject", :branch => "master" } do |d|
   d.artifacts.each { |a| log "Deploying artifact #{artifact.path}" }
 end
