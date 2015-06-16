e8fd6dfd8a15d5c9d3c1ea07e9dd234a228d929f
Michaël Fortin <michael@simbioz.com>
2015-06-15 21:48:07 -0400

    Fetching the latest build that corresponds to the given options from TeamCity. WIP on fetching artifacts.

--- lib/polldeploy/sources/teamcity_source.rb
+++ lib/polldeploy/sources/teamcity_source.rb
@@ -16,8 +16,22 @@ module PollDeploy
 
     def fetch(options)
       configure_teamcity_client
-      project = TeamCity.project(id: options[:project_id])
-      return project
+
+      # Further restrict fetch results
+      options[:branch] = "refs/heads/" + options[:branch] if options[:branch]
+      options[:status] = "SUCCESS"
+      options[:count] = 1
+
+      # Find the latest successful build
+      latest_build = TeamCity.builds(options).first
+      raise "No build found for build locator #{options}" unless latest_build.first
+
+      ServiceLog.log_info(latest_build.inspect)
+
+      artifacts = TeamCity.build_artifacts(latest_build.id)
+      ServiceLog.log_info(artifacts.inspect)
+
+      return artifacts
     rescue Exception => e
       ServiceLog.log_error("Could not fetch source '#{@id}':\n#{e.message}")
     end
